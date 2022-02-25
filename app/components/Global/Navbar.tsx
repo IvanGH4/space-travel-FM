9071b58c62104b0d27fe6af8979cce265a95b39f
Iván García <66884918+IvanGH4@users.noreply.github.com>
2022-02-25 20:50:05 -0300

    Added active link class

--- app/components/Global/Navbar.tsx
+++ app/components/Global/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'remix';
+import { Link, useLocation } from 'remix';
 import styled from 'styled-components';
 import { colors } from '~/utils/styles';
 
@@ -11,7 +11,7 @@ const Nav = styled.nav`
 
 const NavItems = styled.ul`
   display: flex;
-  padding: 39px 165px 38px 123px;
+  padding: 39px 165px 0px 123px;
 
   background: rgba(255, 255, 255, 0.04);
   backdrop-filter: blur(100px);
@@ -19,6 +19,11 @@ const NavItems = styled.ul`
 
   li {
     text-transform: uppercase;
+    padding-bottom: 38px;
+
+    &.active {
+      border-bottom: solid 3px ${colors.light};
+    }
 
     &:not(:last-child) {
       margin-right: 48px;
@@ -33,22 +38,24 @@ const NavItems = styled.ul`
 `;
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
     <Nav>
       <div>
         <img src="/img/logo.svg" alt="Logo" />
       </div>
       <NavItems>
-        <li>
+        <li className={location.pathname === '/' ? 'active' : ''}>
           <Link to="/">00 Home</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/destination' ? 'active' : ''}>
           <Link to="/destination">01 Destination</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/crew' ? 'active' : ''}>
           <Link to="/crew">02 Crew</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/technology' ? 'active' : ''}>
           <Link to="/technology">03 Technology</Link>
         </li>
       </NavItems>
